75173fc3a0917de3929e3600d9a58c703f9c19d4
Bo Li <bl@broadpeakpartners.com>
2019-10-07 16:41:10 -0400

    65

--- src/clojure_practice/core.clj
+++ src/clojure_practice/core.clj
@@ -736,3 +736,29 @@ f vs
                    false
                    (not-any? zero? (map mod (repeat number) (range 3 (/ number 2) 2)))))]
     (cons 2 (take (dec num) (filter prime? (iterate inc 2))))))
+
+; #65: Black Box Testing
+;
+; Clojure has many sequence types, which act in subtly different ways. 
+; The core functions typically convert them into a uniform "sequence" type and work with them that way, 
+; but it can be important to understand the behavioral and performance differences so that you know which kind is appropriate for your application.
+;
+; Write a function which takes a collection and returns one of :map, :set, :list, or :vector - describing the type of collection it was given.
+; You won't be allowed to inspect their class or use the built-in predicates like list? - the point is to poke at them and understand their behavior.
+; 
+; (= :map (__ {:a 1, :b 2}))
+; 
+; (= :list (__ (range (rand-int 20))))
+; 
+; (= :vector (__ [1 2 3 4 5 6]))
+; 
+; (= :set (__ #{10 (rand-int 5)}))
+; 
+; (= [:map :set :vector :list] (map __ [{} #{} [] ()]))
+; solution 1:
+(fn [coll]
+  (let [base (empty coll)]
+    (condp = base
+      #{} :set
+      {} :map
+      (if (reversible? coll) :vector :list))))
