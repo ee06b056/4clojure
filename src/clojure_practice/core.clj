e428cc12cf0b11be91e7ca762ea05e16cd501694
Bo Li <bl@broadpeakpartners.com>
2019-10-08 10:29:34 -0400

    74

--- src/clojure_practice/core.clj
+++ src/clojure_practice/core.clj
@@ -762,3 +762,19 @@ f vs
       #{} :set
       {} :map
       (if (reversible? coll) :vector :list))))
+
+; #74: Filter Perfect Squares
+;
+; Given a string of comma separated integers, write a function which returns a new comma separated string
+; that only contains the numbers which are perfect squares.
+;
+; (= (__ "4,5,6,7,8,9") "4,9")
+;
+; (= (__ "15,16,25,36,37") "16,25,36")
+; solution 1
+(fn [s]
+  (->> s
+       (#(clojure.string/split % #","))
+       (map read-string)
+       (filter #(let [root (Math/sqrt %)] (== root (int root))))
+       (clojure.string/join ",")))
