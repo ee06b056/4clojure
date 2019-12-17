2efc06722e867da41b0d62e9d760a1b934637666
Bo Li <bl@broadpeakpartners.com>
2019-12-17 13:25:51 -0500

    reducer init

--- src/clojure_practice/basic.clj
+++ src/clojure_practice/basic.clj
@@ -1,4 +1,5 @@
-(ns clojure-practice.basic)
+(ns clojure-practice.basic
+  (:refer-clojure))
 
 (defn relay [x i]
   (prn i)
@@ -6,7 +7,6 @@
     (send (:next x) relay i))
   (when (and (zero? i) (:report-queue x))
     (.put (:report-queue x) i))
-  (prn x)
   x)
 
 (defn run [m n]
@@ -17,8 +17,32 @@
       (send hd relay i))
     (.take q)))
 
-(time (run 10 10))
+; (time (run 10 10))
 
-; (def a (reduce (fn [next _] (agent {:next next}))
-;         (agent {:report-queue []})
-;         (range (dec 100))))
+(def yak-butter-international
+  {:store "Yak Butter International"
+   :price 90
+   :smoothness 90})
+(def butter-than-nothing
+  {:store "Butter Than Nothing"
+   :price 150
+   :smoothness 83})
+;; This is the butter that meets our requirements
+(def baby-got-yak
+  {:store "Baby Got Yak"
+   :price 94
+   :smoothness 99})
+
+(defn mock-api-call [result]
+  (Thread/sleep 1000)
+  result)
+
+(defn satisfactory?
+  "If the butter meets our criteria, return the butter, else return false"
+  [butter]
+  (and (<= (:price butter) 100)
+       (>= (:smoothness butter) 97)
+       butter))
+
+(time (some (comp satisfactory? mock-api-call)
+            [yak-butter-international butter-than-nothing baby-got-yak]))
