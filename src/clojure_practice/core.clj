438b5eed7c9b119e3ff9551f601841af195dbb22
Bo Li <bl@broadpeakpartners.com>
2020-08-26 23:13:10 -0400

    4clojure #110

--- src/clojure_practice/core.clj
+++ src/clojure_practice/core.clj
@@ -1156,10 +1156,41 @@ f vs
   (fn [& args]
     (reduce #(%1 %2) f args)))
 
+;; #110: Sequence of pronunciations
+;;
+;; Write a function that returns a lazy sequence of "pronunciations" 
+;; of a sequence of numbers. A pronunciation of each element in the 
+;; sequence consists of the number of repeating identical numbers 
+;; and the number itself. For example, [1 1] is pronounced as [2 1] 
+;; ("two ones"), which in turn is pronounced as [1 2 1 1] ("one two, one one").
+;; 
+;; Your function should accept an initial sequence of numbers, 
+;; and return an infinite lazy sequence of pronunciations, 
+;; each element being a pronunciation of the previous element.
+;;
+;; (= [[1 1] [2 1] [1 2 1 1]] (take 3 (__ [1])))
+;;
+;; (= [3 1 2 4] (first (__ [1 1 1 4 4])))
+;;
+;; (= [1 1 1 3 2 1 3 2 1 1] (nth (__ [1]) 6))
+;;
+;; (= 338 (count (nth (__ [3 2]) 15)))
+(fn lazy-sp [col]
+  (let [next-col (->> col
+                     (partition-by identity)
+                      (mapcat #(list (count %) (first %))))]
+    (lazy-seq
+     (cons next-col (lazy-sp next-col)))))
+
 (comment
   "experiment space"
   (lazy-seq
    )
+  (->> '(1 1 1 2 2 1)
+       (partition-by identity)
+       (mapcat #(list (count %) (first %))))
+  (partition-by identity [1])
+  (cons 1 (cons 2 nil))
   (let [f (fn f [n & [f1 & fs]]
             (lazy-seq
              (cons n (apply f (f1 n) (concat fs (list f1))))))]
